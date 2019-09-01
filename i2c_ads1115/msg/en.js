3f70e06da309cb2188da0da3fbfbc7635fb9d5a4
Kritsadapong  Suta <mr.suta@gmail.com>
2019-09-01 20:54:37 +0700

    Add files via upload

---
+++ i2c_ads1115/msg/en.js
@@ -0,0 +1,7 @@
+Blockly.Msg.I2C_TITLE = "ADS1115";
+Blockly.Msg.I2C_SELECT_TITLE="I2C Port";
+Blockly.Msg.I2C_CHANNEL_TITLE="Channel";
+Blockly.Msg.I2C_ADDRESS_TITLE="Address";
+Blockly.Msg.I2C_INPUT_TITLE="Input";
+Blockly.Msg.I2C_TOOLTIP="KidBright I2C Channel, ADS1115 Address, ADS1115 Input";
+Blockly.Msg.I2C_HELP_URL="http://www.9flife.com/";
---
+++ i2c_ads1115/msg/th.js
@@ -0,0 +1,7 @@
+Blockly.Msg.I2C_TITLE = "ADS1115";
+Blockly.Msg.I2C_SELECT_TITLE="พอร์ต I2C";
+Blockly.Msg.I2C_CHANNEL_TITLE="ช่องเชือมต่อ";
+Blockly.Msg.I2C_ADDRESS_TITLE="ที่อยู้่";
+Blockly.Msg.I2C_INPUT_TITLE="ช่องสัญญาณ";
+Blockly.Msg.I2C_TOOLTIP="ช่องการเชื่อมต่อ I2C ของ Kidbright, ที่อยู่ ADS1115, ช่องสัญญาณของ ADS1115";
+Blockly.Msg.I2C_HELP_URL="http://www.9flife.com/";
